6479af18725ebb329619c0e106186760a4eec01c
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-12-12 12:24:50 +0300

    Create access and refresh tokens

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,23 @@ import { buildSchema } from "type-graphql";
 
 import { userResolvers } from "./resolvers/userResolvers";
 
+// import cors from "cors";
+
 (async () => {
     const app = express();
 
+    const cors = {
+        credentials : true,
+        origin : 'https://studio.apollographql.com'
+    }
+
+    // app.use(
+    //     "/graphql",
+    //     cors({
+    //       origin: 'https://studio.apollographql.com',
+    //       credentials: true,
+    //     })
+    //   );
     const apolloserver = new ApolloServer({
         schema: await buildSchema({
             resolvers: [userResolvers],
@@ -17,7 +31,7 @@ import { userResolvers } from "./resolvers/userResolvers";
     })
 
     await apolloserver.start();
-    apolloserver.applyMiddleware({app: app as express.Application, cors: true})
+    apolloserver.applyMiddleware({app: app , cors})
     app.listen(3000, () => {
         console.log("Connected on port", 3000 )
     })
--- src/resolvers/userResolvers.ts
+++ src/resolvers/userResolvers.ts
@@ -1,6 +1,13 @@
-import { Field, InputType, ObjectType, Resolver, registerEnumType, Int, Mutation, Arg, Query } from "type-graphql";
+import { Field, InputType, ObjectType, Resolver, registerEnumType, Int, Mutation, Arg, Query, Ctx } from "type-graphql";
+
+import { hash, compare } from "bcrypt";
 
 import { PrismaClient, Role, Status } from "@prisma/client";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { MyContext } from "src/MyContext";
+dotenv.config();
+
 const prisma = new PrismaClient();
 
 registerEnumType(Status, {
@@ -28,7 +35,13 @@ class User {
     role : Role
 
     @Field(() => Status)
-    status : Status
+    status : Status 
+
+    @Field()
+    username : string
+
+    @Field()
+    password : string
 }
 
 @InputType()
@@ -43,24 +56,83 @@ class CreateUserInput {
     @Field(() => Role)
     role : Role
 
-    @Field(() => Status, {nullable : true})
-    status? : Status
+    @Field(() => Status)
+    status : Status
+
+    @Field()
+    username: string
+
+    @Field()
+    password: string
+}
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    token : string
+}
+
+function generateToken(user : User, secret : string) : string {
+
+    if(!secret) throw new Error("Secret can not be empty");
+    
+    const token = jwt.sign({role : user.role, status : user.status}, secret, {expiresIn : "15m"})
+
+    return token;
 }
 
 @Resolver()
 export class userResolvers {
     @Mutation(() => User)
     async createAdmin(@Arg("newAdmin", () => CreateUserInput) newAdmin : CreateUserInput) : Promise<User>  {
-
-        const createdAdmin = await prisma.user.create({
-            data : newAdmin
-        })
-
-        return createdAdmin;
+        try{
+            const hashedPass = await hash(newAdmin.password, 10);
+            const updatedAdmin = { ...newAdmin, password : hashedPass};
+    
+            const createdAdmin = await prisma.user.create({
+                data : updatedAdmin
+            })
+    
+            return createdAdmin;
+        }catch(error){
+            console.log(error)
+            return error.message
+        }        
     }
 
     @Query(() => [User])
     async getUsers() {
         return await prisma.user.findMany()
     }
+
+    @Query(() => LoginResponse  )
+    async login(@Arg("username", () => String) username : string,
+                @Arg("password", () => String) password : string,
+                @Ctx() {res} : MyContext) : Promise<LoginResponse> {
+        
+        try{
+
+            const userExists = await prisma.user.findUnique({
+                where : {username}
+            })
+            if(!userExists) throw new Error("Authentication failed")
+
+            const verify = await compare(password, userExists.password)
+            if(!verify) throw new Error("Authentication failed")
+
+            if(!process.env.REFRESH_TOKEN_SECRET || !process.env.ACCESS_TOKEN_SECRET) {
+                throw new Error("Jwt secret can not be undefined");
+            }
+            const refreshToken = generateToken(userExists, process.env.REFRESH_TOKEN_SECRET);
+
+            res.cookie( "theToken", refreshToken, {httpOnly:true} )
+
+            return {token : generateToken(userExists, process.env.ACCESS_TOKEN_SECRET)}
+
+        }catch(error) {
+            console.log(error);
+            return error.message;
+        }
+    } 
+    
 }
